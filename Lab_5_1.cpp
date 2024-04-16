13243ffef4c3bed1d591e04af054cc63600efc2f
LetteR <LetteR@DESKTOP-PBINGPF>
2024-04-16 14:42:39 +0300

    Добавьте файлы проекта.

---
+++ Lab_5_1.cpp
@@ -0,0 +1,15 @@
+﻿#include "Header.h"
+#include "Func.cpp"
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LC_ALL, "rus");
+	
+	chel human[N] = {};
+	for (int i = 0; i < N; i++) {
+		human[i] = Func();
+	}
+	printchel(human);
+	return 0;
+ }
